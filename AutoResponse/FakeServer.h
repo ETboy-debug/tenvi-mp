a68221852860dc6a5616d5a66036b34c7f907e77
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-02 18:45:13 +0800

    add AutoResponse/FakeServer.h

---
+++ AutoResponse/FakeServer.h
@@ -0,0 +1,23 @@
+#ifndef __FAKESERVER_H__
+#define __FAKESERVER_H__
+
+#include"ClientPacket.h"
+#include"ServerPacket.h"
+#include"TenviData.h"
+#include"TemporaryData.h"
+
+// FakeServer.cpp 使用的全局账号数据（定义在 TemporaryData.cpp）
+extern TemporaryData TA;
+
+#define MAPID_ITEM_SHOP 65535
+#define MAPID_PARK 65534
+#define MAPID_EVENT 62501
+
+bool FakeServer(ClientPacket &cp);
+// test
+void WorldListPacket();
+void CharacterSelectPacket();
+void CharacterListPacket();
+void CharacterListPacket_Test();
+
+#endif
