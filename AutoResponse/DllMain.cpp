7f9d2b91687510f7c155f237801f461c430042f2
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-02 19:57:36 +0800

    fix: AutoResponse/DllMain.cpp

--- AutoResponse/DllMain.cpp
+++ AutoResponse/DllMain.cpp
@@ -1,5 +1,6 @@
 #include"AutoResponse.h"
 #include"TenviData.h"
+#include"MPClient.h"
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	if (fdwReason == DLL_PROCESS_ATTACH) {
@@ -7,6 +8,8 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 		LoadRegionConfig(hinstDLL);
 		tenvi_data.set_xml_path(GetXMLPath());
 		AutoResponseHook();
+		// [MP] 启动到独立服务端的明文桥接连接
+		MP_Start(hinstDLL);
 	}
 	return TRUE;
 }
