3f410b825004911330884b361c80df1e983ef108
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-02 21:02:53 +0800

    fix: AutoResponse/FakeServer.h

--- AutoResponse/FakeServer.h
+++ AutoResponse/FakeServer.h
@@ -7,7 +7,9 @@
 #include"TemporaryData.h"
 
 // FakeServer.cpp 使用的全局账号数据（定义在 TemporaryData.cpp）
-extern TenviAccount TA;
+// [MP] 每个连接线程一份独立会话状态, 这样多个玩家同时连服务端不会串号。
+// 客户端 DLL 侧只有主线程访问, 行为不变。
+extern thread_local TenviAccount TA;
 
 #define MAPID_ITEM_SHOP 65535
 #define MAPID_PARK 65534
