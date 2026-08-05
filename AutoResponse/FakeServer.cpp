27b8b2b8bc0240e5e628764d29d7a0c772e3baf7
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-05 18:14:09 +0800

    fix: GM admin port start after WSAStartup

--- AutoResponse/FakeServer.cpp
+++ AutoResponse/FakeServer.cpp
@@ -1149,7 +1149,13 @@ bool FakeServer(ClientPacket &cp) {
 		cp.Decode1(); // 0
 		std::wstring message = cp.DecodeWStr1();
 
-		// command test
+		// [TEST] 触发 NPC 对话: 普通聊天 /npc <id> (绕过 @ 被客户端拦的问题)
+		if (_wcsnicmp(message.c_str(), L"/npc ", 5) == 0) {
+			DWORD npc_id = (DWORD)_wtoi(&message.c_str()[5]);
+			NPCTalkPacket(npc_id, L"[Tenvi MP] NPC ID=" + std::to_wstring(npc_id) + L" - chat command test");
+		}
+
+		// @ 命令 (含 @map/@npc)
 		if (message.length() && message.at(0) == L'@') {
 			if (_wcsnicmp(message.c_str(), L"@map ", 5) == 0) {
 				int map_id = _wtoi(&message.c_str()[5]);
