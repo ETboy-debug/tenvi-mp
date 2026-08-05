2698bddb91fb2f4962784845ca17fdba59830b76
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-05 19:28:58 +0800

    fix: GM admin port start after WSAStartup

--- AutoResponse/FakeServer.cpp
+++ AutoResponse/FakeServer.cpp
@@ -1142,8 +1142,8 @@ bool FakeServer(ClientPacket &cp) {
 #endif
 		// Use object_id as fallback when npc_type is 0
 		DWORD talk_id = (npc_type != 0) ? npc_type : object_id;
-		// Try three candidate opcodes for NPC dialogue response
-		static const BYTE cand[] = {0x50, 0x51, 0x52};
+		// Sweep many candidate opcodes for NPC dialogue response
+		static const BYTE cand[] = {0x0D, 0x0E, 0x14, 0x2A, 0x30, 0x33, 0x38, 0x50, 0x51, 0x52, 0x54};
 		BYTE *hdr = ServerPacket::GetOpcode();
 		BYTE saved = hdr[SP_NPC_TALK];
 		for (BYTE op : cand) {
@@ -1151,12 +1151,12 @@ bool FakeServer(ClientPacket &cp) {
 			ServerPacket sp(SP_NPC_TALK);
 			sp.Encode4(object_id);
 			sp.Encode1(0);
-			sp.EncodeWStr2(L"NPC obj=" + std::to_wstring(object_id) + L" op=0x" + std::to_wstring(op));
+			sp.EncodeWStr2(L"obj=" + std::to_wstring(object_id) + L" op=0x" + std::to_wstring(op));
 			SendPacket(sp);
 		}
 		hdr[SP_NPC_TALK] = saved;
 		// Also show BoardPacket as visual confirmation
-		BoardPacket(Board_Spawn, L"NPC", L"Clicked obj=" + std::to_wstring(object_id));
+		BoardPacket(Board_Spawn, L"NPC", L">>> NPC TALKED obj=" + std::to_wstring(object_id) + L" <<<");
 		return true;
 	}
 	case CP_PLAYER_CHAT: {
