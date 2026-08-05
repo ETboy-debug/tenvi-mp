07052625bc1b6c9eb663bcb0f2a6b3097a7b6796
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-06 01:15:17 +0800

    fix: AutoResponse/CN_v126_CP.cpp

--- AutoResponse/CN_v126_CP.cpp
+++ AutoResponse/CN_v126_CP.cpp
@@ -27,7 +27,7 @@ void SetClientPacketHeader_CN_v126() {
 	header[CP_USE_PORTAL] = 0x4E;
 	header[CP_PLAYER_REVIVE] = 0x4F;
 	header[CP_CHANGE_CHANNEL] = 0x50;
-	header[CP_NPC_MOVEMENT] = 0x52;
+	// header[CP_NPC_MOVEMENT] = 0x52;  // 与 CP_PLAYER_CHAT 冲突(0x52), 注释掉, 聊天命令优先
 	header[CP_NPC_TALK] = 0x53;
 	header[CP_PLAYER_CHAT] = 0x52;
 	header[CP_FRIEND_REQUEST] = 0x74;
