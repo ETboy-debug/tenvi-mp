cd8e259722a73151a5df5ea801e2a3174e49113a
ETboy-debug <wangjun4122@vip.qq.com>
2026-08-05 11:46:40 +0800

    fix: AutoResponse/JP_v127_SP.cpp

--- AutoResponse/JP_v127_SP.cpp
+++ AutoResponse/JP_v127_SP.cpp
@@ -22,6 +22,7 @@ void SetServerPacketHeader_JP_v127() {
 	header[SP_REMOVE_OBJECT] = 0x12;
 	header[SP_CREATE_OBJECT] = 0x14;
 	header[SP_ACTIVATE_OBJECT] = 0x20;
+	header[SP_NPC_TALK] = 0x28;
 	// 0x22 Object Movement
 	header[SP_HIT] = 0x21;
 	header[SP_SHOW_OBJECT] = 0x23;
